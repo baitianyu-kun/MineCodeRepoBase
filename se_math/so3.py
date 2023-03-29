f6e162836ca34b4625316601123f26c89c7ac901
BaiTianyu <1748383809@qq.com>
2023-03-29 20:35:23 +0800

    change so3, add transform_np and transform_torch

--- se_math/so3.py
+++ se_math/so3.py
@@ -244,6 +244,32 @@ def transform(g, a):
     return b
 
 
+def transform_np(g: np.ndarray, pts: np.ndarray):
+    """ Use g to transform a numpy version
+    Args:
+        g: SO3 transformation matrix of size ([B,] 3, 3)
+        pts: Points to be transformed ([B,] N, 3)
+    Returns:
+        transformed points of size (N, 3)
+    """
+    rot = g[..., :3, :3]  # (3, 3)
+    transformed = pts[..., :3] @ np.swapaxes(rot, -1, -2)
+    return transformed
+
+
+def transform_torch(g: torch.Tensor, pts: torch.Tensor):
+    """ Use g to transform a torch version
+    Args:
+        g: SO3 transformation matrix of size ([B,] 3, 3)
+        pts: Points to be transformed ([B,] N, 3)
+    Returns:
+        transformed points of size (N, 3)
+    """
+    rot = g[..., :3, :3]  # (3, 3)
+    transformed = pts[..., :3] @ torch.swapaxes(rot, -1, -2)
+    return transformed
+
+
 def group_prod(g, h):
     # g, h : SO(3)
     g1 = g.matmul(h)
--- test/test_se_math/test_so3.py
+++ test/test_se_math/test_so3.py
@@ -1,12 +1,19 @@
 import torch
+import utils.data_utils as du
 from se_math import so3
+import numpy as np
+from utils.visual import *
+from copy import deepcopy
 
-# mats=torch.rand((2,1,1))
-# print(mats)
-# traces=so3.btrace(mats)
-# print(traces)
-
-rotation_mat = torch.rand((2, 3, 3))
-rotation_mat_inv=so3.inverse(rotation_mat)
-print(rotation_mat)
-print(rotation_mat_inv)
+data = np.loadtxt('../data/airplane_0627.txt', delimiter=',')[:, 0:3]
+data2 = np.loadtxt('../data/airplane_0010.txt', delimiter=',')[:, 0:3]
+rotate_mtx = du.random_rotation(60)
+rotate_mtx_torch = torch.from_numpy(rotate_mtx)
+data_torch = torch.from_numpy(data)
+data2_torch = torch.from_numpy(data2)
+data_2_batch = torch.concat([data_torch.unsqueeze(0), data2_torch.unsqueeze(0)])
+rotate_mtx_2_batch=torch.concat([rotate_mtx_torch.unsqueeze(0),rotate_mtx_torch.unsqueeze(0)])
+transform_origin=so3.transform(rotate_mtx_2_batch,data_2_batch.transpose(1,2)).transpose(1,2)
+transform_torch=so3.transform_torch(rotate_mtx_2_batch,data_2_batch)
+print(transform_origin)
+print(transform_torch)
