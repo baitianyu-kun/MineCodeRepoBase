eb18c0272012763a5022a4768044590aed889312
BaiTianyu <1748383809@qq.com>
2023-04-02 14:11:41 +0800

    add dgcnn transformer

--- test/test_others/test_sprase.py
+++ test/test_others/test_sprase.py
@@ -6,15 +6,32 @@ from models.dgcnn_transformer import knn
 import scipy
 
 data = np.loadtxt('../data/airplane_0010.txt', delimiter=',')[:, 0:3]
-data = du.farthest_avg_subsample_points(data, 50)
+data = du.farthest_avg_subsample_points(data, 5)
 data = torch.from_numpy(data).unsqueeze(0).transpose(1, 2)
+
+data2 = np.loadtxt('../data/airplane_0627.txt', delimiter=',')[:, 0:3]
+data2 = du.farthest_avg_subsample_points(data2, 5)
+data2 = torch.from_numpy(data2).unsqueeze(0).transpose(1, 2)
+
+data_batch2=torch.concat([data,data2])
+
 knn_k = 4
 idx = knn(data, knn_k)
 edgelist = idx[0]
-num_nodes = 50
+num_nodes = len(edgelist)
 adj = np.zeros((num_nodes, num_nodes))
 print(edgelist)
 # 第0号中[0, 47, 37, 29]，说明第0号和0, 47, 37, 29相连接(0和0，即包括他自己)
-for i in range(len(edgelist)):
+for i in range(num_nodes):
     for j in edgelist[i]:
         adj[i][j] = 1
+
+# edgelist([[0, 4, 3, 2],
+#         [1, 2, 3, 0],
+#         [2, 1, 0, 4],
+#         [3, 0, 1, 2],
+#         [4, 0, 2, 3]]) -> adj2[i][0,4,3,2]=1 is ok too
+adj2 = np.zeros((num_nodes, num_nodes))
+for i in range(num_nodes):
+    adj2[i][edgelist[i]] = 1
+
---
+++ test/test_others/test_sprase_b2.py
@@ -0,0 +1,27 @@
+import numpy as np
+import torch
+import utils.data_utils as du
+import utils.visual as vi
+from models.dgcnn_transformer import knn
+import scipy
+
+data = np.loadtxt('../data/airplane_0010.txt', delimiter=',')[:, 0:3]
+data = du.farthest_avg_subsample_points(data, 5)
+data = torch.from_numpy(data).unsqueeze(0).transpose(1, 2)
+
+data2 = np.loadtxt('../data/airplane_0627.txt', delimiter=',')[:, 0:3]
+data2 = du.farthest_avg_subsample_points(data2, 5)
+data2 = torch.from_numpy(data2).unsqueeze(0).transpose(1, 2)
+
+data_batch2 = torch.concat([data, data2])
+
+knn_k = 4
+# idx (batch, num_pts, knn_k)
+idx = knn(data_batch2, knn_k)
+edge_list = idx
+batches, num_nodes, _ = edge_list.shape
+adj = np.zeros((batches, num_nodes, num_nodes))
+for batch in range(batches):
+    for i in range(num_nodes):
+        adj[batch][i][edge_list[batch][i]] = 1
+
